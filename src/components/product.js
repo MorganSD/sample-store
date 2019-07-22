7c7b8996e7c25961a50c0b1be099f2b340ac18d2
bahardehghani@bahars-Air <bahar.6es@gmail.com>
2019-07-22 22:03:40 +0430

    add redux and comma npm

--- src/components/product.js
+++ src/components/product.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Addbasket from "../Icon Simplestore/Asset.png";
 import Rate from "./rate";
 import noPhoto from "../Icon Simplestore/noPhoto.png";
+import {connect} from 'react-redux';
+
 
 const Thumbnail = (props) =>{
     if (props.thumbnail != null) {
@@ -11,9 +13,11 @@ const Thumbnail = (props) =>{
       return <img src={noPhoto} />;
     }
   }
-
+ 
 
 const Product = (props) =>{
+  var commaNumber = require('comma-number')
+
     return(
         <div className="itemBox">
               <figure>
@@ -32,7 +36,7 @@ const Product = (props) =>{
                 <p>
                   <Link to={`/item/${props.product.address}`}>{props.product.title}</Link>
                 </p>
-                <p>{props.product.price} تومان</p>
+                <p>{commaNumber(props.product.price)} تومان</p>
 
                 <div className="rate">
                   <Rate product={props.product} />
@@ -41,4 +45,11 @@ const Product = (props) =>{
             </div>
     )
 }
-export default Product;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItemToCart : (item) => {dispatch({type: 'ADD_TO_BASKET', item: item})}
+   
+
+  }
+}
+export default connect(null , mapDispatchToProps)(Product);
