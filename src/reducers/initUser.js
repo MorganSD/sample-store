6c9b4a04b0cc23cf8cb22f928ada376179aa0e55
bahardehghani@bahars-Air <bahar.6es@gmail.com>
2019-07-22 22:04:17 +0430

    update redux reducers

--- src/reducers/initUser.js
+++ src/reducers/initUser.js
@@ -1,77 +1,90 @@
-import axios from '../axios';
+import axios from "../axios";
 
-
-const initialState =  {
-   
-     cardDisplay : false,
-     currentUser : [],
-     cardProduct :[]
-}
+const initialState = {
+  cardDisplay: false,
+  currentUser: [],
+  cardProduct: [],
+  totalPrice: 0,
+  favouriteDisplay: false,
+  favouriteProducts: []
+};
 
 const InitUserReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INIT_GUEST": {
+      axios.post("/profiles/guest/").then(res => {
+        localStorage.setItem("jwtToken", JSON.stringify(res.data.data));
+      });
+      let guset = JSON.parse(localStorage.getItem("jwtToken"));
+      return {
+        ...state,
+        currentUser: guset
+      };
+    }
 
-     switch(action.type) {
-          case 'INIT_GUEST': {
-               axios.post("/profiles/guest/").then(res => {
-                    localStorage.setItem("jwtToken", JSON.stringify(res.data.data))  
-               })
-               let guset = JSON.parse(localStorage.getItem('jwtToken')) 
-               return {
-                    ...state,
-                    currentUser : guset
-               
-               }
-          }
-            
-          case 'INIT_LOGIN_USER':
-           {
-               return {
-                    ...state,
-                    currentUser : JSON.parse(localStorage.getItem('jwtToken')) 
-               }  
-           }
-          case 'CARD_DISPLAY':
-               {
-                    const display = !state.cardDisplay
-     return {
+    case "INIT_LOGIN_USER": {
+      return {
+        ...state,
+        currentUser: JSON.parse(localStorage.getItem("jwtToken"))
+      };
+    }
+    case "CARD_DISPLAY": {
+      const display = !state.cardDisplay;
+      return {
+        ...state,
+        cardDisplay: display
+      };
+    }
+    case "ADD_TO_BASKET": {
+      let product = initialState.cardProduct;
+      let currentTotalPrice = 0;
+      if (!product.find(item => item.address === action.item.address)) {
+        product.push(action.item);
+        console.log("mahsolate sabade kharid", product);
+        product.map(item => (currentTotalPrice += item.price));
+
+        return {
           ...state,
-          cardDisplay : display
+          cardProduct: product,
+          totalPrice: currentTotalPrice
+        };
+      } else {
+        alert("این محصول در سبد خرید شما قبلا ثیت شده است");
+        return {
+          ...state
+        };
+      }
+    }
+    case "FAVOURITE_DISPALY": {
+      const display = !state.favouriteDisplay;
+      return {
+        ...state,
+        favouriteDisplay: display
+      };
+    }
+    case "ADD_TO_FAVOURITE": {
+      let productt = state.favouriteProducts;
 
-     }
-          }
-            
-          default:
-            return state;
-        }
+      if (!(productt.find(item => item.address === action.item.address))) {
+        productt.push(action.item);
+        console.log("mahsolate morede alaghe", productt);
 
-
-
-//   if (action.type === "INIT_GUEST") {
-//           axios.post("/profiles/guest/").then(res => {
-//           localStorage.setItem("jwtToken", JSON.stringify(res.data.data))  
-//      })
-//      let guset = JSON.parse(localStorage.getItem('jwtToken')) 
-//      return {
-//           ...state,
-//           currentUser : guset
-     
-//      }
-// }else if (action.type === 'INIT_LOGIN_USER'){
-//      return {
-//           ...state,
-//           currentUser : JSON.parse(localStorage.getItem('jwtToken')) 
-//      }
-// }else if (action.type === 'CARD_DISPLAY'){
-//      const display = !state.cardDisplay
-//      return {
-//           ...state,
-//           cardDisplay : display
-
-//      }
-// }
-
-
-// return state;
-}
+        return {
+          ...state,
+          favouriteProducts: productt
+        };
+      } else {
+        let updateFavourites = productt.filter(item => item.address != action.item.address)
+        console.log('update  fav' , updateFavourites)
+        return {
+          ...state,
+          favouriteProducts : updateFavourites
+        };
+      }
+    }
+    default:
+      return state;
+  }
+};
 
 export default InitUserReducer;
