cd39d53506a94b88e1e8b88f30c484881d45afa1
bahardehghani@bahars-Air <bahar.6es@gmail.com>
2019-07-30 20:57:34 +0430

    Authorization token

--- src/axios.js
+++ src/axios.js
@@ -1,23 +1,48 @@
 import axios from 'axios';
-import { promises } from 'fs';
+import {connect} from 'react-redux';
+
+// const token = JSON.parse(localStorage.getItem("jwtToken")).token;
 
 const instance = axios.create({
-    baseURL : 'http://api.projectant.aasoo.ir'
+    baseURL : 'http://api.projectant.aasoo.ir',
+    // headers : { Authorization: `Token e4eb7493f0814fb9fc724b02c9284c1f848a188b`}
 })
+
 
 instance.defaults.headers.common = {
     "Content-Type": "application/x-www-form-urlencoded",
     "x-api-key": "0f855b9c2f5ee2a21e530bcaa82a645286724fba",
     accept: "application/json",
-    "x-store-sub-address": "sib"
+    "x-store-sub-address": "sib",
+
 }
 
 instance.interceptors.request.use(request => {
-    console.log(request)
+    
+    if (!request.headers.Authorization) {
+        const token = JSON.parse(localStorage.getItem("jwtToken")).token;
+        console.log('token',token)
+        if (token) {
+          request.headers.Authorization = `Token ${token}`;
+
+        }
+      }else{
+        console.log('!token')
+
+      }
+  
     return request
 },error => {
     console.log(error)
     
 }
 )
-export default instance;
+
+
+const mapStateToProps = state => {
+    return { token: state.InitUserReducer.currentUser.token };
+  };
+
+export default connect(
+    mapStateToProps
+  )(instance);
