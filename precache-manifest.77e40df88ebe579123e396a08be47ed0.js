48ba7ef144faf4be643a3997e7627caf1e3360c5
bahardehghani@bahars-Air <bahar.6es@gmail.com>
2019-08-21 11:44:24 +0430

    Updates

---
+++ precache-manifest.77e40df88ebe579123e396a08be47ed0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19541a3e3d5049437abdfe13f400c9df",
+    "url": "/sample-store/index.html"
+  },
+  {
+    "revision": "600fa68324a4da7cfb87",
+    "url": "/sample-store/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "134c51ba2120e85f1734",
+    "url": "/sample-store/static/css/main.893b31ef.chunk.css"
+  },
+  {
+    "revision": "600fa68324a4da7cfb87",
+    "url": "/sample-store/static/js/2.f7c14896.chunk.js"
+  },
+  {
+    "revision": "134c51ba2120e85f1734",
+    "url": "/sample-store/static/js/main.b3d74b36.chunk.js"
+  },
+  {
+    "revision": "acdcb256761adc75f0a6",
+    "url": "/sample-store/static/js/runtime~main.93efb4dd.js"
+  },
+  {
+    "revision": "c3e570ebe4614c480e10fb762ebff21b",
+    "url": "/sample-store/static/media/Asset-white.c3e570eb.png"
+  },
+  {
+    "revision": "70fbe50e583a0e847bc371b0a3393d90",
+    "url": "/sample-store/static/media/Asset.70fbe50e.png"
+  },
+  {
+    "revision": "f7361ed91d344ca6ef9afd3e40088feb",
+    "url": "/sample-store/static/media/Group 771.f7361ed9.png"
+  },
+  {
+    "revision": "ac1755ef723126e3caf5df090a798247",
+    "url": "/sample-store/static/media/formaloo-01.ac1755ef.png"
+  },
+  {
+    "revision": "0974083c5903ee783c6f9a1afd132df1",
+    "url": "/sample-store/static/media/iranyekanwebregular.0974083c.woff"
+  },
+  {
+    "revision": "74500baf775f2f1832ffd1653acf73b4",
+    "url": "/sample-store/static/media/iranyekanwebregular.74500baf.eot"
+  },
+  {
+    "revision": "e4f82d0d75dcf22c36fd01481ea9effc",
+    "url": "/sample-store/static/media/iranyekanwebregular.e4f82d0d.ttf"
+  },
+  {
+    "revision": "8e9cec9b8f2fa2c48cae188ed9f29807",
+    "url": "/sample-store/static/media/like.8e9cec9b.png"
+  },
+  {
+    "revision": "990b3edf41d4f814979a3a690236e6d9",
+    "url": "/sample-store/static/media/like2.990b3edf.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-store/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "210272344c90c2d6545f192936eb7b4f",
+    "url": "/sample-store/static/media/noPhoto.21027234.png"
+  },
+  {
+    "revision": "0586bdadd12409f6e791dbe3f28a171b",
+    "url": "/sample-store/static/media/search.0586bdad.png"
+  },
+  {
+    "revision": "9814e76510afe68d97b70e4e7340dcd4",
+    "url": "/sample-store/static/media/shopping-cart (2).9814e765.png"
+  },
+  {
+    "revision": "a0ffb106f9108dc972772838dae5e22c",
+    "url": "/sample-store/static/media/star (1).a0ffb106.png"
+  },
+  {
+    "revision": "d62f4a478aebf373c0363effdeb968bb",
+    "url": "/sample-store/static/media/star.d62f4a47.png"
+  }
+]);
